80b562dc3e1573ee697a3aa5c06072150b5a295e
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-28 17:02:59 -0500

    trying out some highlighting

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const config = eleventyConfig => {
   eleventyConfig.addPassthroughCopy('styles')
   eleventyConfig.addPassthroughCopy({ './node_modules/tachyons/css/tachyons.css': 'vendor/tachyons.css' })
   eleventyConfig.addPassthroughCopy({ './node_modules/colors.css/css/colors.css': 'vendor/colors.css' })
+  eleventyConfig.addPassthroughCopy({ './node_modules/@tommijonasar/ft-syntax-highlight/src/ft-syntax-highlight.css': 'vendor/syntax.css' })
 }
 
 module.exports = config
